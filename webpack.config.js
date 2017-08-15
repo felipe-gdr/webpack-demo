7c33a66e7a9418c75c7ce63b6ae6ef9e7d5730ff
Felipe Reis <freis@atlassian.com>
2017-08-15 20:29:13 +1000

    Finished styling/loading section

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('require');
+const merge = require('webpack-merge');
+
+const parts = require('./webpack.parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-const commonConfig = {
-  // Entries have to resolve to files! They rely on Node
-  // convention by default so if a directory contains *index.js*,
-  // it resolves to that.
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo',
-    }),
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        eslint: {
-          // Fail only on errors
-          failOnWarning: false,
-          failOnError: true,
-
-          // Toggle autofix
-          fix: false,
-
-          // Output to Jenkins compatible XML
-          outputReport: {
-            filePath: 'checkstyle.xml',
-            formatter: require('eslint/lib/formatters/checkstyle'),
-          },
-        },
-      },
-    }),
-  ],
-};
-
-const productionConfig = () => commonConfig;
-
-const developmentConfig = () => {
-  const config = {
-    devServer: {
-      // Enable history API fallback so HTML5 History API based
-      // routing works. Good for complex setups.
-      historyApiFallback: true,
-
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
-
-      // Parse host and port from env to allow customization.
-      //
-      // If you use Docker, Vagrant or Cloud9, set
-      // host: options.host || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices
-      // unlike default `localhost`.
-      host: process.env.HOST, // Defaults to `localhost`
-      port: process.env.PORT, // Defaults to 8080
-      // overlay: true is equivalent
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
     },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          enforce: 'pre',
-          loader: 'eslint-loader',
-          options: {
-            emitWarning: true,
-          },
-        },
-      ],
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
     },
-  };
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
+    ],
+  },
+  parts.lintJavaScript({ include: PATHS.app }),
+  parts.loadCSS(),  
+]);
 
-  return Object.assign(
-    {},
-    commonConfig,
-    config
-  );
-};
+const productionConfig = merge([
+]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
 
 module.exports = (env) => {
-  console.log('env', env);
-
-  if(env === 'production') {
-    return productionConfig();
-  } else {
-    return developmentConfig();
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
   }
+
+  return merge(commonConfig, developmentConfig);
 };
---
+++ webpack.parts.js
@@ -0,0 +1,55 @@
+exports.devServer = ({ host, port }) => ({
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. Good for complex setups.
+    historyApiFallback: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host,
+    port,
+    // overlay: true is equivalent
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
+
+exports.lintJavaScript = ({ include, exclude, options }) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        enforce: 'pre',
+
+        loader: 'eslint-loader',
+        options,
+      },
+    ],
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+  
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+});
